dd61654ab1628e88e9e313b42ed89c3e315a129c
Nazina <nazina096@gmail.com>
2025-11-20 20:29:17 +0530

    chnages tin functin name in test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,9 @@
 import os
 from models import predict,load_model
 
-def check_model_exist():
+def test_check_model_exist():
     assert os.path.exists("model.pkl")
 
-def check_predictiion():
+def test_check_predictiion():
     pred=predict([8.5,40.0,6.1,1.2,320,2.55,37.4,-120.33])
     assert isinstance(pred,float)
--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,19 +1,19 @@
 import numpy as np
 import pytest
 from preprocessing import min_max
-def check_values():
+def test_check_values():
     arr=np.array([10,20,30])
     result=min_max(arr)
     assert result.min()==0
     assert result.max()==1
 
-def check_negative():
+def test_check_negative():
     arr=np.array([-5,8,-2])
     result=min_max(arr)
     assert result.min()==0
     assert result.max()==1
 
 
-def check_zero_size_arrray():
+def test_check_zero_size_arrray():
     with pytest.raises(ValueError):
         min_max(np.array([]))
